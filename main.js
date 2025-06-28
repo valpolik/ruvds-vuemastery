dfaf275d73afcec53e9437d6ee1b9ab7ea6dd8b1
Valentin Polikarpov <valpolik@gmail.com>
2025-06-28 07:29:54 +0300

    Lesson 2. Done: Added image with Alt Text

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        // image: "./assets/vmSocks-green.jpg",
+        image: "./assets/vmSocks-blue.jpg",
+        altText: "A pair of socks",
     }
 })
