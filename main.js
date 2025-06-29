c75b44aeed883ebc166b63c542c3c3fb9208fd1d
Valentin Polikarpov <valpolik@gmail.com>
2025-06-29 05:45:39 +0300

    Lesson 2. Practice, home work

--- main.js
+++ main.js
@@ -5,5 +5,6 @@ var app = new Vue({
         // image: "./assets/vmSocks-green.jpg",
         image: "./assets/vmSocks-blue.jpg",
         altText: "A pair of socks",
+        link: "https://www.amazon.com/s?k=socks&crid=PHJVN071W1N4&sprefix=socks%2Caps%2C221&ref=nb_sb_noss_1",
     }
 })
