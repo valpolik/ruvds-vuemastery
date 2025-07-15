c34eac9c89b10e214f4efaf1027aad7f2d6a8bc3
Valentin Polikarpov <valpolik@gmail.com>
2025-07-15 22:20:23 +0300

    Lesson 4. Done.

--- main.js
+++ main.js
@@ -6,5 +6,15 @@ var app = new Vue({
         // image: "./assets/vmSocks-blue.jpg",
         inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'    
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            },
+        ],
     }
 })
