6eb8fe8524fb67ef6508f18b4d68ddaf1ea7f4d8
Valentin Polikarpov <valpolik@gmail.com>
2025-07-15 21:19:43 +0300

    Lesson 3. Done: Added v-else-if

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var app = new Vue({
         image: "./assets/vmSocks-green.jpg",
         // image: "./assets/vmSocks-blue.jpg",
         inStock: true,
+        inventory: 100,
         altText: "A pair of socks",
         link: "https://www.amazon.com/s?k=socks&crid=PHJVN071W1N4&sprefix=socks%2Caps%2C221&ref=nb_sb_noss_1",
     }
