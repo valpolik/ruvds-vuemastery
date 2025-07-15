ddbb8b11026a5edbc4dcf4f3c5bb308549359afe
Valentin Polikarpov <valpolik@gmail.com>
2025-07-15 20:45:48 +0300

    Lesson 3. Done: Initial if else

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ var app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        // image: "./assets/vmSocks-green.jpg",
-        image: "./assets/vmSocks-blue.jpg",
+        image: "./assets/vmSocks-green.jpg",
+        // image: "./assets/vmSocks-blue.jpg",
+        inStock: true,
         altText: "A pair of socks",
         link: "https://www.amazon.com/s?k=socks&crid=PHJVN071W1N4&sprefix=socks%2Caps%2C221&ref=nb_sb_noss_1",
     }
