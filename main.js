a4f0e8d8c50775b0dc8c24dbe27a82b556d7c100
Valentin Polikarpov <valpolik@gmail.com>
2025-07-18 22:14:56 +0300

    Lesson 7. Replaced image property with computed image property

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ var app = new Vue({
     data: {
         product: "Socks",
         brand: 'Vue Mastery',
-        image: "./assets/vmSocks-green.jpg",
+        // image: "./assets/vmSocks-green.jpg",
         // image: "./assets/vmSocks-blue.jpg",
+        selectedVariant: 0,
         inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
@@ -25,13 +26,16 @@ var app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
         },
     },
     computed: {
         title() {
             return this.brand + ' ' + this.product;
-        }
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
     },
 })
