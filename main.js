144c4f9397b1bcd2b7cc3b6a798b5a44c191cc71
Valentin Polikarpov <valpolik@gmail.com>
2025-07-19 00:11:21 +0300

    Lesson 7. Home work

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ var app = new Vue({
             },
         ],
         cart: 0,
+        onSale: true,
     },
     methods: {
         addToCart() {
@@ -41,6 +42,13 @@ var app = new Vue({
         },
         inStock() {
             return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' are on sale!'
+            }
+
+            return  this.brand + ' ' + this.product + ' are not on sale'
         },
     },
 })
