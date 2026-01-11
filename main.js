a1281a7871d6ed6cad69124eeb6ad2544ff28455
Valentin Polikarpov <valpolik@gmail.com>
2026-01-12 01:00:38 +0300

    Lesson 9. Moved cart from product-component to root-component.

--- main.js
+++ main.js
@@ -31,10 +31,6 @@ Vue.component('product', {
                 >
                     Add to cart
                 </button>
-
-                <div class="cart">
-                    <p>Cart({{ cart }})</p>
-                </div>
             </div>
 
         </div>
@@ -65,12 +61,11 @@ Vue.component('product', {
                     variantQuantity: 0,
                 },
             ],
-            cart: 0,
         };
     },
     methods: {
         addToCart() {
-            this.cart += 1;
+            this.$emit('add-to-cart');
         },
         updateProduct(index) {
             this.selectedVariant = index;
@@ -99,6 +94,12 @@ Vue.component('product', {
 var app = new Vue({
     el: '#app',
     data: {
-        premium: true
-    }
+        premium: true,
+        cart: 0,
+    },
+    methods: {
+        updateCart() {
+            this.cart += 1;
+        },
+    },
 });
