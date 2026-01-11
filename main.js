350a1ddb2c31905d6825568902a326104f3526de
Valentin Polikarpov <valpolik@gmail.com>
2026-01-12 01:11:27 +0300

    Lesson 9. Make cart to store ids of added products.

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ Vue.component('product', {
     },
     methods: {
         addToCart() {
-            this.$emit('add-to-cart');
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
         },
         updateProduct(index) {
             this.selectedVariant = index;
@@ -95,11 +95,11 @@ var app = new Vue({
     el: '#app',
     data: {
         premium: true,
-        cart: 0,
+        cart: [],
     },
     methods: {
-        updateCart() {
-            this.cart += 1;
+        updateCart(id) {
+            this.cart.push(id);
         },
     },
 });
